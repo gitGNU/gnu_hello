b2ea35e841762f907dc3c02bcd897460cf2856a5
karl <>
2007-06-26 16:04:37 +0000

    [project @ 2007-06-26 16:04:37 by karl] update notices for GPLv3

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,11 @@
 /* hello.c -- print a greeting message and exit.
 
    Copyright (C) 1992, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
-   2005, 2006 Free Software Foundation, Inc.
+   2005, 2006, 2007 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
+   the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
