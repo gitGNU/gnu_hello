2a58ebad1f9029d0ae24f86a8e2391fd40232d34
karl <>
2007-06-26 16:17:26 +0000

    [project @ 2007-06-26 16:17:26 by karl] --version notice

--- src/hello.c
+++ src/hello.c
@@ -186,7 +186,8 @@ print_version (void)
      year comes around.  */
   printf (_("\
 Copyright (C) %s Free Software Foundation, Inc.\n\
-License: GNU GPL v2+ <http://www.gnu.org/licenses/gpl.html>\n\
-This is free software.  There is NO WARRANTY, to the extent permitted by law.\n"),
-              "2006");
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n"),
+              "2007");
 }
