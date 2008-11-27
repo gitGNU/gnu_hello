5cda38c24e62948bddb7fc76f6d99ee235ab4a7f
karl <>
2008-11-27 19:55:06 +0000

    [project @ 2008-11-27 19:55:01 by karl] url instead of physical FSF address

--- src/system.h
+++ src/system.h
@@ -1,5 +1,6 @@
 /* system.h: system-dependent declarations; include this first.
-   Copyright (C) 1996, 2005, 2006, 2007 Free Software Foundation, Inc.
+
+   Copyright 1996, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -12,8 +13,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef HELLO_SYSTEM_H
 #define HELLO_SYSTEM_H
