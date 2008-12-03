f33017dce273b035338aa055ae4852ab8bd8fa9f
karl <>
2008-12-03 18:49:06 +0000

    [project @ 2008-12-03 18:49:06 by karl] it is 2008

--- src/hello.c
+++ src/hello.c
@@ -199,5 +199,5 @@ Copyright (C) %s Free Software Foundation, Inc.\n\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"),
-              "2007");
+              "2008");
 }
