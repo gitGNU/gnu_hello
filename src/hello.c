059d8d620ce038c575fe4c9605ca0fb377099718
Sami Kerola <kerolasa@iki.fi>
2013-10-09 20:20:16 +0100

    make copyright year pseudo dynamic
    configure.ac: Add macro to determine what year is it, and add the value
    to config.h.
    hello.c: Use copyright year from config.h.

--- src/hello.c
+++ src/hello.c
@@ -200,8 +200,8 @@ print_version (void)
      as done here, to avoid having to retranslate the message when a new
      year comes around.  */
   printf (_("\
-Copyright (C) %s Free Software Foundation, Inc.\n\
+Copyright (C) %d Free Software Foundation, Inc.\n\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n"), "2011");
+There is NO WARRANTY, to the extent permitted by law.\n"), COPYRIGHT_YEAR);
 }
