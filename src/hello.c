74f297e42baa5a464b92a0164933ebe5872f039c
Sami Kerola <kerolasa@iki.fi>
2015-01-01 10:47:09 +0000

    maint: update all copyright year number ranges
    Run "make update-copyright" and
    
    * configure.ac: Add copyright header.
    * contrib/de_franconian_po.txt: Translate copyright sign ascii
    character to capital C in round braces in order to get the file to be
    part of "make update-copyright" scope.

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,8 @@
 /* hello.c -- print a greeting message and exit.
 
    Copyright 1992, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2005,
-   2006, 2007, 2008, 2010, 2011, 2013, 2014 Free Software Foundation,
-   Inc.
+   2006, 2007, 2008, 2010, 2011, 2013, 2014, 2015 Free Software
+   Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- src/system.h
+++ src/system.h
@@ -1,7 +1,7 @@
 /* system.h: system-dependent declarations; include this first.
 
-   Copyright 1996, 2005, 2006, 2007, 2008, 2013, 2014 Free Software
-   Foundation, Inc.
+   Copyright 1996, 2005, 2006, 2007, 2008, 2013, 2014, 2015 Free
+   Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
